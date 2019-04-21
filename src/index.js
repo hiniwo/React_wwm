27783d40031a482c92b1bbbdf743bb1dd6b3c6c1
walker <hiniwoo@163.com>
2019-04-21 10:39:38 +0800

    初始化

--- src/index.js
+++ src/index.js
@@ -4,7 +4,72 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// function Welcome(props) {
+// 	return <h1>Hello, {props.name}</h1>;
+// }
+//
+// const element = <Welcome name="Saras" />;
+// ReactDOM.render(
+// 	element,
+// 	document.getElementById('root')
+// );
+
+
+class Clock extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			date: new Date(),
+			isToggleOn : true
+			
+		};
+		this.handleClick = this.handleClick.bind(this);
+	}
+	
+	componentDidMount() {
+		this.timerID = setInterval(
+			() => this.tick(),
+			1000
+		);
+	}
+	
+	componentWillUnmount() {
+		clearInterval(this.timerID);
+	}
+	
+	tick() {
+		this.setState({
+			date: new Date()
+		});
+	}
+	
+	handleClick() {
+		this.setState(prevState => ({
+			isToggleOn: !prevState.isToggleOn
+		}));
+	}
+	
+	
+	render() {
+		return (
+			<div>
+				<h1>Hello, world!</h1>
+				<h1>React,project</h1>
+				<button onClick={this.handleClick}>
+					{this.state.isToggleOn ? 'ON' : 'OFF'}
+				</button>
+				<h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+			</div>
+		);
+	}
+}
+
+ReactDOM.render(
+	<Clock />,
+	document.getElementById('root')
+);
+
+// ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
