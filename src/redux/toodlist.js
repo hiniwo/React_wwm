730c647aa58b772ef8d9c18de80ab890149cd1cd
walker <hiniwoo@163.com>
2019-11-22 23:55:19 +0800

    redux增删以及优化dispatch

--- src/redux/toodlist.js
+++ src/redux/toodlist.js
@@ -2,11 +2,14 @@ import 'antd/dist/antd.css'
 import React, { Component } from 'react';
 import { Input , Button , List } from 'antd';
 import store from '../store/index'
+
+import {changeInputAction , addItemAction , deleteItemAction} from '../store/actionCreators'
 class ToodList extends Component {
     constructor(props){
         super(props)
         this.state = store.getState();
-        this.changeInputValue= this.changeInputValue.bind(this)
+        this.changeInputValue = this.changeInputValue.bind(this)
+        this.addList = this.addList.bind(this)
         this.storeChange = this.storeChange.bind(this)  //转变this指向
         store.subscribe(this.storeChange) //订阅Redux的状态,关键代码
     }
@@ -17,27 +20,35 @@ class ToodList extends Component {
                     <Input placeholder={this.state.inputValue} 
                         onChange={this.changeInputValue}
                         style={{ width:'250px', marginRight:'10px'}}/>
-                    <Button type="primary">增加</Button>
+                    <Button type="primary" onClick={this.addList}>增加</Button>
                 </div>
                 <div style={{margin:'10px',width:'300px'}}>
                     <List
                         bordered
                         dataSource={this.state.list}
-                        renderItem={item=>(<List.Item>{item}</List.Item>)}
+                        renderItem={(item , index)=>(
+                        <List.Item onClick={this.deleteList.bind(this , index)}
+                        >{item}
+                        </List.Item>)}
                     />    
                 </div>
             </div>
          );
     }
     changeInputValue(e){
-        const action ={
-            type:'changeInput',
-            value:e.target.value
-        }
+        const action = changeInputAction(e.target.value)
         store.dispatch(action);
     }
     storeChange(){
         this.setState(store.getState())
+    }
+    addList(){
+        const action = addItemAction()
+        store.dispatch(action)
+    }
+    deleteList(index){
+        const action = deleteItemAction(index)
+        store.dispatch(action)
     }
 }
  
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -5,11 +5,22 @@ const defaultState = {
         '中午下班游泳一小时' ]
 }  //默认数据
 export default (state = defaultState,action)=>{  //就是一个方法函数
-    console.log(state,action)
     if(action.type === 'changeInput'){
         let newState = JSON.parse(JSON.stringify(state)) //深度拷贝state
         newState.inputValue = action.value
         return newState
+    }
+
+    if(action.type === 'addItem'){
+        let newState = JSON.parse(JSON.stringify(state)) //深度拷贝state
+        newState.list.push(newState.inputValue);
+        return newState
+    }
+
+    if(action.type === 'deleteItem'){
+        let newState = JSON.parse(JSON.stringify(state)) //深度拷贝state
+        newState.list.splice(action.index , 1);
+        return newState;
     }
     return state
 }
