1167a69d2654534580d020482bd1d051d1235031
walker <hiniwoo@163.com>
2019-11-28 22:22:21 +0800

    路由嵌套

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,8 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Index from './pages/index'
-import List from './pages/list'
-import Home from './pages/home'
+// import List from './pages/list'
+// import Home from './pages/home'
+
+import './index.css'
+
+import Video from './pages/video'
 
 
 // function Index() {
@@ -12,19 +16,47 @@ import Home from './pages/home'
 // function List() {
 //     return <h2>List-Page</h2>;
 // }
+
+//单路由部分
   
-  function AppRouter() {
-    return (
-      <Router>
-          <ul>
-              <li> <Link to="/">首页</Link> </li>
-              <li><Link to="/list/123">列表</Link> </li>
-          </ul>
-          <Route path="/" exact component={Index} />
-          <Route path="/list/:id" component={List} />
-          <Route path="/home/" component={Home} />
+  // function AppRouter() {
+  //   return (
+  //     <Router>
+  //         <ul>
+  //             <li> <Link to="/">首页</Link> </li>
+  //             <li><Link to="/list/123">列表</Link> </li>
+  //         </ul>
+  //         <Route path="/" exact component={Index} />
+  //         <Route path="/list/:id" component={List} />
+  //         <Route path="/home/" component={Home} />
         
-      </Router>
-    );
+  //     </Router>
+  //   );
+  // }
+
+
+  //嵌套路由部分
+
+
+  function AppRouter () {
+      return (
+          <Router>
+            <div className="mainDiv">
+              <div className="leftNav">
+                  <h3>一级导航</h3>
+                  <ul>
+                      <li> <Link to="/">博客首页</Link> </li>
+                      <li><Link to="/video/">视频教程</Link> </li>
+                      <li><Link to="">职场技能</Link> </li>
+                  </ul>
+              </div>
+              
+              <div className="rightMain">
+                  <Route path="/"  exact component={Index} />
+                  <Route path="/video/"   component={Video} />
+              </div>
+            </div>
+        </Router>
+      )
   }
   export default AppRouter;
